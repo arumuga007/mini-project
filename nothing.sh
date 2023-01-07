45e6f8b8554f1266c019029657b6673cb51751e7
arumuga007 <116297050+arumuga007@users.noreply.github.com>
2023-01-07 17:35:01 +0530

    Add files via upload

---
+++ nothing.sh
@@ -0,0 +1,12 @@
+echo "Checking prime no or not"
+echo "Enter a number"
+read n
+case $n in
+13) echo "The number is prime" ;;
+5) echo "The number is prime" ;;
+7) echo "The number is prime" ;;
+17) echo "The number is prime" ;;
+19) echo "The number is prime" ;;
+*) echo "The number is not prime" ;;
+esac
+hii
